63b1145a73e6e37d5abe0e552911bcd62ba85d13
YogishaNiraula <yogishaniraula@gmail.com>
2023-03-09 14:04:57 +0545

    chore: indexedDb functions and services

--- src/utils/task.js
+++ src/utils/task.js
@@ -1,10 +1,20 @@
 import { openDB } from "idb";
 import { getProject, getProjects } from "./project";
 
-export async function createTask({ project_id, task }) {
+async function withProjectDB(fn, mode = "readwrite") {
   try {
     const db = await openDB("Projects", 1);
-    const tx = db.transaction("projects", "readwrite");
+    const tx = db.transaction("projects", mode);
+    const output = await fn(tx);
+    await tx.done;
+    return output;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function createTask({ project_id, task }) {
+  return await withProjectDB(async (tx) => {
     let project = await tx.store.get(project_id);
     project?.tasks.push({
       id: task.id,
@@ -14,11 +24,40 @@ export async function createTask({ project_id, task }) {
     });
 
     await tx.store.put(project, project_id);
-    let response = await tx.store.get(project_id);
-    console.log(response);
-    await tx.done;
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
+    return await tx.store.get(project_id);
+  });
+}
+
+export async function deleteTask({ project_id, task_id }) {
+  return await withProjectDB(async (tx) => {
+    let project = await tx.store.get(project_id);
+    await tx.store.put(
+      {
+        ...project,
+        tasks: project.tasks.filter((task) => task.id != task_id),
+      },
+      project_id,
+    );
+    return await tx.store.get(project_id);
+  });
+}
+
+export async function editTask({ project_id, task_data }) {
+  return await withProjectDB(async (tx) => {
+    let project = await tx.store.get(project_id);
+    await tx.store.put(
+      {
+        ...project,
+        tasks: project.tasks.map((task) => {
+          if (task.id === task_data.edit_id) {
+            task.description = task_data.edit_description;
+            task.title = task_data.edit_title;
+          }
+          return task;
+        }),
+      },
+      project_id,
+    );
+    return await tx.store.get(project_id);
+  });
 }
