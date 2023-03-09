3b048c7b03a1418cdb62d9a64aead5040351ee5c
YogishaNiraula <yogishaniraula@gmail.com>
2023-03-09 14:04:57 +0545

    chore: indexedDb functions and services

--- src/utils/project.js
+++ src/utils/project.js
@@ -1,12 +1,14 @@
-import { openDB, wrap } from "idb";
+import { openDB } from "idb";
 
 export async function createProject(project_name) {
   let id = Math.random().toString(36).substring(2, 9);
   const db = await openDB("Projects", 1);
   const tx = db.transaction("projects", "readwrite");
-  await tx.store.add({ id: id, name: project_name, tasks: [] }, id);
+  const response = await tx.store.add(
+    { id: id, name: project_name, tasks: [] },
+    id,
+  );
 
-  let response = await getProjects();
   await tx.done;
   return response;
 }
@@ -21,8 +23,24 @@ export async function getProjects() {
 
 export async function getProject(id) {
   const db = await openDB("Projects", 1);
-  const tx = db.transaction("projects", "readwrite");
+  const tx = db.transaction("projects", "readonly");
   let response = await tx.store.get(id);
+  await tx.done;
+  return response;
+}
+
+export async function editProject({ id, project_data }) {
+  const db = await openDB("Projects", 1);
+  const tx = db.transaction("projects", "readwrite");
+  let response = await tx.store.put({ id: id, name: project_data.name }, id);
+  await tx.done;
+  return response;
+}
+
+export async function deleteProject(id) {
+  const db = await openDB("Projects", 1);
+  const tx = db.transaction("projects", "readwrite");
+  let response = await tx.store.delete(id);
   await tx.done;
   return response;
 }
